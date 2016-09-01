6d5cf805d489f8f943ab4a14329432c9002c3db5
Andrii Pindiura <aydnep@aydnep.com.ua>
2016-09-01 22:09:05 +0300

    change forEach to for loop

--- form/script/form.js
+++ form/script/form.js
@@ -13,9 +13,15 @@ document.addEventListener('DOMContentLoaded', function () {
 			e.preventDefault();
 		}
 	});
-	document.querySelectorAll('input').forEach(function (input) {
-		input.addEventListener('change', function (event) { inputValidate(event.srcElement); });
-	});
+	var inputs = document.querySelectorAll('input');
+	for (var i = 0, l = inputs.length; i < l; i++) {
+		inputs[i].addEventListener('change', function (event) { inputValidate(event.srcElement); });
+	}
+	console.log(inputs);
+	// inputs.forEach(function (input) {
+	// 	console.log(input);
+	// 	input.addEventListener('change', function (event) { inputValidate(event.srcElement); });
+	// });
 
 	function inputValidate(element) {
 		var mailRegexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
